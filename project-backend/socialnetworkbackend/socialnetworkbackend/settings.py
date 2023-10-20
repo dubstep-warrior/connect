9381d2c714eb31c1b4ba588725b545a98283108d
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-10-20 17:45:27 +0800

    Update allowed hosts

--- project-backend/socialnetworkbackend/socialnetworkbackend/settings.py
+++ project-backend/socialnetworkbackend/socialnetworkbackend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!F
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["backend.dubstep-warrior.com"]
 
 
 # Application definition
