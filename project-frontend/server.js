be919f2b19193552d2cf9c8aba612502de6f7a33
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-10-20 15:59:09 +0800

    Update build path

--- project-frontend/server.js
+++ project-frontend/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/dist/project-frontend"));
+app.use(express.static(__dirname + "/dist/"));
 
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
 
