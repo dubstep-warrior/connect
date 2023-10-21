0e3a734d736b2a14f03af3d1240b90c3854d491a
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-10-22 01:25:44 +0800

    Update wss regex

--- project-backend/socialnetworkbackend/chat/routing.py
+++ project-backend/socialnetworkbackend/chat/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 
 # NOTE USE OF ws/ to separate out our ws URIs, like rest use of api/
 websocket_urlpatterns = [
-    re_path(r'ws/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'(ws|wss)/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
