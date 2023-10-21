98d9773566dbbc7a042e3f9e6c1a40b3c8819ab1
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-10-22 03:51:01 +0800

    Update originvalidator

--- project-backend/socialnetworkbackend/socialnetworkbackend/routing.py
+++ project-backend/socialnetworkbackend/socialnetworkbackend/routing.py
@@ -1,15 +1,16 @@
 from chat.middlewares import TokenAuthMiddleWare
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import OriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': TokenAuthMiddleWare(
-    AllowedHostsOriginValidator(
+    OriginValidator(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
-    )
+    ),
+    ["https://app.dubstep-warrior.com"],
     ),
 })
