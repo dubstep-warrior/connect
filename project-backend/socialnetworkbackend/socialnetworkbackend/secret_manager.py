45c8321c29ae87accb7595898867af84ce1199c1
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-10-22 20:50:31 +0800

    pls work

--- project-backend/socialnetworkbackend/socialnetworkbackend/secret_manager.py
+++ project-backend/socialnetworkbackend/socialnetworkbackend/secret_manager.py
@@ -17,9 +17,15 @@ def get_secret(name, key):
     region_name = "ap-southeast-1" 
 
     try: 
+        try:
+            aws_access_key_id = env(
+            'AWS_ACCESS_KEY_ID')
+            aws_secret_access_key = env('AWS_SECRET_ACCESS_KEY')
+        except: 
+            return env(key)
+
         # Create a Secrets Manager client
-        session = boto3.session.Session(aws_access_key_id=env(
-            'AWS_ACCESS_KEY_ID'), aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY'))
+        session = boto3.session.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
         client = session.client(
             service_name='secretsmanager',
             region_name=region_name
@@ -31,7 +37,7 @@ def get_secret(name, key):
         # For a list of exceptions thrown, see
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         print('client err', e)
-        return env(key)
+        return None
 
     # Decrypts secret using the associated KMS key.
     secret = get_secret_value_response['SecretString']
